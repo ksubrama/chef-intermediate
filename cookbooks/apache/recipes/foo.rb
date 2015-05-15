76fdbdf97478afd3141182d6d42e334c40f0abea
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-05-15 15:33:52 -0400

    Play with LWRP attributes.

---
+++ cookbooks/apache/recipes/foo.rb
@@ -0,0 +1,4 @@
+apache_vhost "Something" do
+  action :create
+  test_arg "foo"
+end
--- cookbooks/apache/resources/vhost.rb
+++ cookbooks/apache/resources/vhost.rb
@@ -1,5 +1,6 @@
 actions :create, :remove
 default_action :create
 
-attribute :site_name, :name_attribute => true, :kind_of => String
+attribute :site_name, :name_attribute => true, :kind_of => String, :required => true
 attribute :site_port, :default => 80, :kind_of => Fixnum
+attribute :test_arg, :required => true, :kind_of => String
